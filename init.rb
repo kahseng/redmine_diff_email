dafc975d5a42500eaa987fbf3d922ba0cfe452bf
Kah Seng Tay <kahseng@gmail.com>
2010-11-20 15:08:08 -0800

    Added README with installation instructions.

--- init.rb
+++ init.rb
@@ -4,22 +4,12 @@ require 'redmine'
 require 'changeset_patch'
 
 Redmine::Plugin.register :redmine_redmine_diff_email do
-  name 'Redmine Redmine Diff Email plugin, fork of http://github.com/genbit/redmine_diff_email'
-  author 'Sergey Generalov <sergey@genbit.ru>'
-  description 'This is a plugin for Redmine'
-  version '0.0.2'
+  name 'Redmine Diff Email Plugin'
+  author 'Kah Seng Tay, Sergey Generalov, Lamar'
+  description 'This is a plugin for Redmine that sends diff emails on commits.'
+  version '0.0.3'
 end
 
 Dispatcher.to_prepare do
   Changeset.send(:include, ChangesetPath)
 end
-
-
-# TODO: Check, is it right?
-#ProjectCustomField.find_by_name("Send Diff Emails") ||
-#  ProjectCustomField.create(:name => "Send Diff Emails",
-#                            :field_format => "bool",
-#                            :is_required => true,
-#                            :is_for_all => false,
-#                            :default_value => nil
-#                            )
