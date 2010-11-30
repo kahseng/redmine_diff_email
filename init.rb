e4252a39441e5cbae27f2185af9f70a09e41c55e
Ivan Evtuhovich <i.evtuhovich@scalaxy.ru>
2010-11-30 10:48:52 +0300

    Add settings description to README, add myself to authors :-)

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require 'changeset_patch'
 
 Redmine::Plugin.register :redmine_redmine_diff_email do
   name 'Redmine Diff Email Plugin'
-  author 'Kah Seng Tay, Sergey Generalov, Lamar'
+  author 'Kah Seng Tay, Sergey Generalov, Lamar, Ivan Evtuhovich'
   description 'This is a plugin for Redmine that sends diff emails on commits.'
   version '0.0.4'
 end
